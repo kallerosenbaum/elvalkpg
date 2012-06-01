6e38defbaf7d1c27edff9259fd38bc17d671ba87
Kalle Rosenbaum <kalle@rosenbaum.se>
2012-06-01 13:55:07 +0200

    Forgot Printer

---
+++ src/main/java/se/elva/lkpg/twitterdemo/Printer.java
@@ -0,0 +1,11 @@
+package se.elva.lkpg.twitterdemo;
+
+public class Printer {
+	public static void p(Object s) {
+		System.out.println(s);
+	}
+	
+	public static void e(Object s) {
+		System.err.println(s);
+	}
+}
