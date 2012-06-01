08a6f50d0c4bbf64cd7ff4f2fd0b86ffd4fd8748
Kalle Rosenbaum <kalle@rosenbaum.se>
2012-06-01 11:30:52 +0200

    Generified tweet-cache, cleaned up imports

--- src/main/java/se/elva/lkpg/twitterdemo/CacheCreator.java
+++ src/main/java/se/elva/lkpg/twitterdemo/CacheCreator.java
@@ -4,9 +4,10 @@ import org.infinispan.Cache;
 import org.infinispan.configuration.cache.CacheMode;
 import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.configuration.global.GlobalConfigurationBuilder;
-import org.infinispan.manager.CacheManager;
 import org.infinispan.manager.DefaultCacheManager;
 import org.infinispan.manager.EmbeddedCacheManager;
+
+import twitter4j.Tweet;
 
 public class CacheCreator {
 	private static EmbeddedCacheManager manager  = new DefaultCacheManager(GlobalConfigurationBuilder.defaultClusteredBuilder()
@@ -33,7 +34,7 @@ public class CacheCreator {
 		configureCache("index-cache");
 	}
 
-	public static Cache<Object, Object> getTweetCache() {
+	public static Cache<Long, Tweet> getTweetCache() {
 		return manager.getCache("tweet-cache");
 	}
 
--- src/main/java/se/elva/lkpg/twitterdemo/DroolsTest.java
+++ src/main/java/se/elva/lkpg/twitterdemo/DroolsTest.java
@@ -15,6 +15,8 @@ import org.drools.logger.KnowledgeRuntimeLoggerFactory;
 import org.drools.runtime.StatefulKnowledgeSession;
 import org.infinispan.Cache;
 
+import twitter4j.Tweet;
+
 /**
  * This is a sample class to launch a rule.
  */
@@ -29,7 +31,7 @@ public class DroolsTest {
 					.newStatefulKnowledgeSession();
 			KnowledgeRuntimeLogger logger = KnowledgeRuntimeLoggerFactory
 					.newFileLogger(ksession, "test");
-			Cache<Object, Object> cache = CacheCreator.getTweetCache();
+			Cache<Long, Tweet> cache = CacheCreator.getTweetCache();
 			TwitterListener twitterListener = new TwitterListener(ksession);
 			cache.addListener(twitterListener);
 			logger.close();
