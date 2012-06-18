698224fab684d55684e27ed888e7b556a84d10fa
Kalle Rosenbaum <kalle@rosenbaum.se>
2012-06-18 13:38:39 +0200

    Moved cache configuration to domain.xml

--- common/src/main/java/se/elva/lkpg/twitterdemo/common/CacheCreator.java
+++ common/src/main/java/se/elva/lkpg/twitterdemo/common/CacheCreator.java
@@ -1,18 +1,11 @@
 package se.elva.lkpg.twitterdemo.common;
 
-import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import javax.ejb.Stateless;
 
 import org.apache.log4j.Logger;
 import org.infinispan.Cache;
-import org.infinispan.configuration.cache.CacheMode;
-import org.infinispan.configuration.cache.Configuration;
-import org.infinispan.configuration.cache.ConfigurationBuilder;
 import org.infinispan.manager.EmbeddedCacheManager;
-import org.infinispan.transaction.LockingMode;
-import org.infinispan.transaction.lookup.JBossTransactionManagerLookup;
-import org.infinispan.transaction.lookup.TransactionManagerLookup;
 
 import twitter4j.Tweet;
 
@@ -25,39 +18,12 @@ public class CacheCreator {
 
 	public static final String TWEET_CACHE = "tweet-cache";
 	public static final String INDEX_CACHE = "index-cache";
-	public static final String TIMER_CACHE = "timer-cache";
 	public static final String TWEET_MAXID_CACHE = "tweet-maxid-cache";
+	/*
+	 * This cache has to use eager cluster-wide locking!
+	 */
+	public static final String TIMER_CACHE = "timer-cache";
 	
-	@SuppressWarnings(value="unused")
-	@PostConstruct
-	private void configureCaches() {
-		logger.debug("Configuring caches");
-		ConfigurationBuilder configBuilder = new ConfigurationBuilder();
-		configBuilder.clustering().cacheMode(CacheMode.DIST_SYNC).hash()
-				.numOwners(2).groups();
-		configBuilder.invocationBatching().enable();
-		Configuration config = configBuilder.build();
-		
-		manager.defineConfiguration(TWEET_CACHE, config);
-		manager.defineConfiguration(TWEET_MAXID_CACHE, config);
-
-		configBuilder = new ConfigurationBuilder();
-		configBuilder.clustering().cacheMode(CacheMode.DIST_ASYNC).hash()
-				.numOwners(2).groups();
-		configBuilder.invocationBatching().enable();
-        manager.defineConfiguration(INDEX_CACHE, configBuilder.build());
-		
-
-		configBuilder = new ConfigurationBuilder();
-		configBuilder.clustering().cacheMode(CacheMode.DIST_SYNC).hash()
-				.numOwners(2).groups();
-		configBuilder.invocationBatching().enable();
-        configBuilder.transaction().lockingMode(LockingMode.PESSIMISTIC);
-		TransactionManagerLookup lookup = new JBossTransactionManagerLookup();
-		configBuilder.transaction().transactionManagerLookup(lookup);
-		manager.defineConfiguration(TIMER_CACHE, configBuilder.build());
-	}
-
 	public Cache<Long, Tweet> getTweetCache() {
 		return manager.getCache(TWEET_CACHE);
 	}
