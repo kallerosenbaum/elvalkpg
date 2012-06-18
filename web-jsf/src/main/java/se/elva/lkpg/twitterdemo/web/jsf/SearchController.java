17493410a6c5ccffa0637433764e6889bf3997a7
Philip Frising <philip.frising@elva.se>
2012-06-18 14:14:29 +0200

    Added new web UI module web-jsf.

---
+++ web-jsf/src/main/java/se/elva/lkpg/twitterdemo/web/jsf/SearchController.java
@@ -0,0 +1,74 @@
+package se.elva.lkpg.twitterdemo.web.jsf;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.infinispan.Cache;
+
+import se.elva.lkpg.twitterdemo.common.CacheCreator;
+import se.elva.lkpg.twitterdemo.common.LuceneStuff;
+import twitter4j.Tweet;
+
+@Named
+@RequestScoped
+public class SearchController {
+
+	@Inject
+	private LuceneStuff luceneStuff;
+
+	@Inject
+	private CacheCreator cacheCreator;
+
+	private String searchFor;
+
+	private String greeting;
+
+	private final List<Tweet> tweets = new ArrayList<Tweet>();
+
+	public void greet() {
+		if (searchIsEntered()) {
+			greeting = "Search results for: \"" + searchFor + "\":";
+			List<Long> matches = luceneStuff.listStoredValuesMatchingString(searchFor,
+					100);
+			populateTweets(matches);
+		} else {
+			greeting = "";
+		}
+	}
+
+	private void populateTweets(List<Long> tweetIds) {
+		tweets.clear();
+		Cache<Long, Tweet> tweetCache = cacheCreator.getTweetCache();
+		for (Long id : tweetIds) {
+			Tweet tweet = tweetCache.get(id);
+			if (tweet != null) {
+				tweets.add(tweet);
+			}
+		}
+	}
+
+	private boolean searchIsEntered() {
+		return searchFor != null && !searchFor.isEmpty();
+	}
+
+	public String getSearchFor() {
+		return searchFor;
+	}
+
+	public void setSearchFor(String searchFor) {
+		this.searchFor = searchFor;
+	}
+
+	public String getGreeting() {
+		return greeting;
+	}
+
+	public List<Tweet> getTweets() {
+		return tweets;
+	}
+
+}
---
+++ web-jsf/src/main/java/se/elva/lkpg/twitterdemo/web/jsf/SubjectController.java
@@ -0,0 +1,98 @@
+package se.elva.lkpg.twitterdemo.web.jsf;
+
+import javax.annotation.PostConstruct;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.infinispan.Cache;
+
+import se.elva.lkpg.twitterdemo.common.CacheCreator;
+import se.elva.lkpg.twitterdemo.common.CacheKeys;
+
+@Named
+@RequestScoped
+public class SubjectController {
+
+	@Inject
+	private CacheCreator cacheCreator;
+
+	private String newSubject;
+
+	private String response;
+
+	private String subjects;
+
+	private Cache<String, String> cache;
+
+	@PostConstruct
+	public void postConstruct() {
+		cache = cacheCreator.getTweetMaxIdCache();
+		addSubjectToDB();
+	}
+
+	public void addSubject() {
+		if (subjectIsEntered()) {
+			addSubjectToDB();
+			response = "Adding subject \"" + newSubject
+					+ "\"! Now following subjects are followed: " + subjects;
+			newSubject = "";
+		} else {
+			response = "Please add subject!";
+		}
+	}
+
+	private void addSubjectToDB() {
+		subjects = getSubjects();
+		if (subjects == null) {
+			subjects = "";
+		}
+
+		// if (subjectAlreadyExists()) {
+		// return;
+		// }
+
+		if (newSubject != null && !"".equals(newSubject)) {
+			if (!"".equals(subjects)) {
+				subjects += ",";
+			}
+			subjects += newSubject;
+		}
+		cache.put(CacheKeys.TWITTER_SUBJECTS, subjects);
+	}
+
+	private boolean subjectAlreadyExists() {
+		String[] subjectArray = subjects.split(",");
+		for (String subject : subjectArray) {
+			if (newSubject.equals(subject)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private String getSubjects() {
+		return cache.get(CacheKeys.TWITTER_SUBJECTS);
+	}
+
+	public void clearSubjects() {
+		cache.put(CacheKeys.TWITTER_SUBJECTS, "");
+	}
+
+	private boolean subjectIsEntered() {
+		return newSubject != null && !newSubject.isEmpty();
+	}
+
+	public String getNewSubject() {
+		return newSubject;
+	}
+
+	public void setNewSubject(String newSubject) {
+		this.newSubject = newSubject;
+	}
+
+	public String getResponse() {
+		return response;
+	}
+
+}
